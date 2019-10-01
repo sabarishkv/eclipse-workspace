154f60bee20946f120e1abc76a1f78f353261a8b
sabarishkv <sabarishk98@gmail.com>
2019-10-02 02:04:58 +0530

    Pushing all files

---
+++ Webdriver/src/webdriver/Floatvalue.java
@@ -0,0 +1,16 @@
+package webdriver;
+import java.util.Scanner;
+
+public class Floatvalue {
+
+	public static void main(String[] args) {
+		Scanner flvalue = new Scanner(System.in);
+		System.out.println("Enter an Float value");
+		float i = flvalue.nextFloat();
+		System.out.println("value of i=="+i );
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
