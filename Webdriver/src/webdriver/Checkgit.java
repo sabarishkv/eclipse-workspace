0c88e769e4470fc52590e0d0bd19fad986e97554
sabarishkv <sabarishk98@gmail.com>
2019-10-02 02:16:32 +0530

    Adding all files again

--- Webdriver/src/webdriver/Checkgit.java
+++ Webdriver/src/webdriver/Checkgit.java
@@ -1,15 +1,12 @@
 package webdriver;
-import java.util.Scanner;
 
 public class Checkgit {
 
 	public static void main(String[] args) {
-		Scanner flvalue = new Scanner(System.in);
-		System.out.println("Enter an Float value");
-		float i = flvalue.nextFloat();
-		System.out.println("value of i=="+i );
+		System.out.println("Push to GIT ");
 		
 		// TODO Auto-generated method stub
+		
 
 	}
 
